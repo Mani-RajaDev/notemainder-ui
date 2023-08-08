58bab78af5e2b2539c0e480abe3d53480721ceb1
devwizard-Mani <orewazorodha@gmail.com>
2023-08-08 15:10:06 +0530

    completed homepage layout and design

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
         "primary": "var(--font-primary)",
         "secondary": "var(--font-secondary)",
       },
+      animation: {
+        "typing": "typing 3.5s steps(40, end), blink-caret .75s step-end infinite"
+      },
+      keyframes: {
+        typing: {
+          "0%": {width: "0px"},
+          "100%": {width: "100%"},
+        },
+        "blink-caret": {
+          "from, to": {"border-color": "transparent"},
+          "50%": {"border-color": "rgb(139,92,246)"}
+        }
+      }
     },
   },
   plugins: [],
