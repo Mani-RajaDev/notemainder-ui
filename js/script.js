cd4925412c6dc6db387a2c38be24c684a7f9af6c
devwizard-Mani <orewazorodha@gmail.com>
2023-08-18 10:46:33 +0530

    partial settings page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
-let themeToggler = document.getElementById('theme__toggler');
-let themeMenu = document.getElementById('theme__menu');
-
 let navBarToggler = document.getElementById('navbar__toggler');
 let navBar = document.getElementById('navbar');
 
-// Theme toggler
-themeToggler.addEventListener('click', () => {
-    if (!navBar.classList.contains('hidden')) navBar.classList.add('hidden');
-    themeMenu.classList.toggle('hidden');
-});
-
 // Menu toggler
 navBarToggler.addEventListener('click', () => {
-    if (!themeMenu.classList.contains('hidden')) themeMenu.classList.add('hidden');
     navBar.classList.toggle('hidden');
 });
-
